f3c638087598579a916b8f1a05674ea38abb50d0
DylanSoule <dylan.soule@icloud.com>
2026-09-17 09:24:33 -0600

    Big commit(I haven't been good at each change commits)
    - Branch init for cpp terminal based smtp client
    - Remove python based parts in branch
      - Remove python file, and images
    - Set up nix development flake to be able to use c++ compiler and libraries
    - Tasks.json for running and compiling withing vscode

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dev environment for email client c++ usage";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      # include necessary packages 
+      packages = with pkgs; [
+        gcc
+	gdb
+	gnumake
+	cmake
+	boost
+      ];
+
+      shellHook = ''
+        code .
+      '';
+    };
+  };
+}
